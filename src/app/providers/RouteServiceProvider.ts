fb7ed77a09a026dc4bacd21e53400c4fdb6fb612
ihooni <to@ihooni.com>
2020-01-14 15:48:47 -0500

    chore: set the basic middlewares

--- src/app/providers/RouteServiceProvider.ts
+++ src/app/providers/RouteServiceProvider.ts
@@ -1,10 +1,24 @@
 import express, { Express } from 'express'
 import mainRouter from '@@/routes/api'
+import helmet from 'helmet'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 
 export default class RouteServiceProvider {
+  /**
+   * Basic middleware list which is globally applied to router.
+   */
+  private static basicMiddleware = [
+    helmet(),
+    cors(),
+    bodyParser.json(),
+    bodyParser.urlencoded({ extended: true })
+  ]
+
   public static boot(): Express {
     const app = express()
     app.set('trust proxy', 1)
+    app.use(this.basicMiddleware)
     app.use('/v1', mainRouter)
 
     return app
