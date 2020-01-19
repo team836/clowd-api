66aef4ce98d4cbd5a0ddadb9a10e245760ac204e
RyuJaesang <rjs9611@naver.com>
2020-01-19 12:20:50 -0500

    modify mac complete

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,17 +1,17 @@
 import express from 'express'
 import passport from 'passport'
-import Google from 'passport-google-oauth20'
+import { Strategy } from 'passport-google-oauth20'
 
 const users = express.Router()
-const GoogleStrategy = Google.Strategy()
 
-passport.use(
-  new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/login/google/callback'
-  })
-)
+// passport.use(
+//   new Strategy({
+//     clientID: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     callbackURL: 'http://localhost:3000/login/google/callback'
+//   }. ())
+// )
+
 
 users.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' })
