f2c7d929d691768f64534401c76e918808bdf282
RyuJaesang <rjs9611@naver.com>
2020-02-04 19:06:34 -0500

    feat: setting mac

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -7,20 +7,20 @@ import UserController from '@/http/controller/UserController'
 
 const users = express.Router()
 
-passport.use(
-  new GoogleStrategy(
-    {
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/v1/users/auth/google/callback',
-  passReqToCallback: true
-    },
-function(req, accessToken, refreshToken, profile, done) {
-  User.findOrCreate({ googleId: profile.id }, function(err, user) {
-        return done(err, user)
-  })
-}))
-)
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//   clientID: process.env.GOOGLE_CLIENT_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   callbackURL: 'http://localhost:3000/v1/users/auth/google/callback',
+//   passReqToCallback: true
+//     },
+// function(req, accessToken, refreshToken, profile, done) {
+//   User.findOrCreate({ googleId: profile.id }, function(err, user) {
+//         return done(err, user)
+//   })
+// }))
+// )
 
 users.get('/checkUsers', UserController.checkUsers)
 
